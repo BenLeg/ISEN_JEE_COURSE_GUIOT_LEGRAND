99417fd6353b622722801f27b33250a0b10795ba
Benjamin L <benjamin.legrand@isen.yncrea.fr>
2020-01-28 12:05:49 +0100

    TrashButton - JEE course - 28_01_2020

--- lab04-web/src/main/java/yncrea/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/yncrea/lab04/web/controller/CompanyController.java
@@ -1,9 +1,14 @@
 package yncrea.lab04.web.controller;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import sun.misc.Request;
 import yncrea.lab04.core.entity.Company;
 import yncrea.lab04.core.service.CompanyService;
 
@@ -14,6 +19,8 @@ public class CompanyController {
 
     private CompanyService companyService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompanyController.class);
+
     public CompanyController(CompanyService companyService) {
         this.companyService = companyService;
     }
@@ -23,5 +30,12 @@ public class CompanyController {
         final List<Company> allCompanyWithProjects = companyService.findAllWithProjects();
         modelMap.addAttribute("companies",allCompanyWithProjects);
         return "companiesList";
+    }
+
+    @RequestMapping(path = "{id}/delete",method = RequestMethod.DELETE)
+    public String deleteOneCompany(@PathVariable Long id){
+        companyService.deleteById(id);
+        LOGGER.debug("The company has been deleted from the database");
+        return "redirect:/list";
     }
 }
