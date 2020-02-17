39ee9dc8d60811c9c4dff4da4f819a84180fce36
Benjamin Legrand <benjamin.legrand@isen.yncrea.fr>
2020-02-17 09:16:21 +0100

    lab 07 source files - 17_02_2020

---
+++ yncrea_2019_lab07/lab07-web/src/main/java/yncrea/lab07/web/controller/RevealController.java
@@ -0,0 +1,36 @@
+package yncrea.lab07.web.controller;
+
+import yncrea.lab07.web.utils.SessionManager;
+
+import java.io.IOException;
+
+@ServerEndpoint(
+        value = "/control",
+        decoders = { SlideDecoder.class },
+        encoders = { SlideEncoder.class })
+public class RevealController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RevealController.class);
+
+
+    @OnOpen
+    public void open(Session s) throws IOException, EncodeException {
+        SessionManager.getInstance().addSession(s);
+        SessionManager.getInstance().sendToSession(s, CurrentSlideHolder.getInstance().getCurrentSlide());
+    }
+
+    @OnClose
+    public void close(Session s){
+        SessionManager.getInstance().removeSession(s);
+    }
+
+
+    @OnMessage
+    public void onSlide(Slide slide, Session session) {
+        CurrentSlideHolder.getInstance().setCurrentSlide(slide);
+        SessionManager.getInstance().broadcast(slide);
+    }
+
+
+
+}
